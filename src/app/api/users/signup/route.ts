51b63e2b621500211300a0d2a3fdf0548e2d83c1
CodeWithBati <xyaan.bati@gmail.com>
2024-04-01 00:59:12 +0500

    routes

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -33,7 +33,7 @@ export async function POST(request: NextRequest) {
     const savedUser = await newUser.save();
 
     // Send Verification Email
-    await sendEmail({ email, emailType: "RESET", userId: savedUser._id });
+    await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id });
 
     return NextResponse.json({
       message: "User register successfully",
