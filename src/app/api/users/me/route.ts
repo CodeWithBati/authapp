99be338defac45c427abffc2c1d0162ee15683d7
CodeWithBati <xyaan.bati@gmail.com>
2024-04-02 00:16:31 +0500

    backend complete

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,23 @@
+import { connect } from "@/db/dbConfig";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import { getDataToken } from "@/helpers/getDataFromToken";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  const userID = await getDataToken(request);
+  const user = await User.findOne({ _id: userID }).select("-password");
+
+  if (!user) {
+    return NextResponse.json({
+      error: "User not found",
+      status: 400,
+    });
+  }
+
+  return NextResponse.json({
+    message: " User Found",
+    data: user,
+  });
+}
