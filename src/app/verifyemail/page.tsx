11370fe45ff741729b5b042f92f282474853c16a
CodeWithBati <xyaan.bati@gmail.com>
2024-04-04 23:28:01 +0500

    veri

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import toast, { Toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export default function VerifyEmailPage() {
-  return (
-    <div>
+  const [token, setToken] = useState("");
+  const [verified, setVerified] = useState(false);
+  const [error, setError] = useState(false);
+
+  const verifyUserEmail = async () =>{
+    try {
+      await axios.post("/api/users/verifyemail", {token});
+    } catch (error) {
       
-    </div>
-  )
+    }
+  }
+
+  return <div></div>;
 }
-
-
-
